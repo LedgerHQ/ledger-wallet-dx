3330927ec9d54074c7885c47101408bb16fa5776
Mario ROBERT <mario.robert@ledger.fr>
2022-10-20 17:16:27 +0200

    [NO-ISSUE] Add nvm/pnpm to ansible
    Node is now installed through nvm and not directly anymore.
    This change allows to easily control node version.

--- bin/macos_bootstrap.sh
+++ bin/macos_bootstrap.sh
@@ -3,5 +3,5 @@
 echo "\n -- Installing Ansible... --\n"
 brew install ansible
 
-echo "\n -- Configuring the Mac... --\n" 
+echo "\n -- Configuring the Mac... --\n"
 ansible-playbook ../ansible_osx.yml
